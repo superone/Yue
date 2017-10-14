3e9ce65b209c8bed9854c8957139f68422805213
Bo Chen <CHENBX@legal.regn.net>
2017-10-15 00:17:54 +0800

    resolve

--- src/yue-class/specifier/resolve/index.js
+++ src/yue-class/specifier/resolve/index.js
@@ -1,3 +1,16 @@
-export function res ( key , value ){
-    
+import util from "../../util";
+import result from "./result";
+
+export function resOpt ( key , value ){
+
+    let resed = util.resOptKey( key );
+    let opt = result();
+
+    opt.value = value;
+    opt.key = key;
+    opt.type = util.getType( value );
+    opt.res = resed;
+    opt.private = util.inArr( 'Private' , resed );
+    opt.const = util.inArr('Const' , resed );
+    opt.overwrite = util.inArr('Overwrite' , resed);
 }
